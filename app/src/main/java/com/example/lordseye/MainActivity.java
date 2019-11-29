f840209ea4937b254a69e208806febd0116a9e81
Priteshkamde <priteshkamde24@gmail.com>
2019-11-29 14:32:54 +0530

    UI -> Main, Driver, Operator Login/Reg pages

--- app/src/main/java/com/example/lordseye/MainActivity.java
+++ app/src/main/java/com/example/lordseye/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private Button driver,Operator;
